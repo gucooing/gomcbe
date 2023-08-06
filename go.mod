e43c16cd507f70d11f7604a8da0875fcfbced108
ノエルの犬 <m15779029379@163.com>
2023-08-06 22:00:50 +0800

    gomcbe

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gucooing/gomcbe
+
+go 1.20
---
+++ pkg/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+type Config struct {
+	ServerPath string `json:"ServerPath"`
+}
+
+var CONF *Config = nil
+
+func GetConfig() *Config {
+	return CONF
+}
+
+var FileNotExist = errors.New("config file not found")
+
+func LoadConfig() error {
+	filePath := "./config.json"
+	if len(os.Args) > 1 {
+		filePath = os.Args[1]
+	}
+	f, err := os.Open(filePath)
+	if err != nil {
+		return FileNotExist
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+	c := new(Config)
+	d := json.NewDecoder(f)
+	if err := d.Decode(c); err != nil {
+		return err
+	}
+	CONF = c
+	return nil
+}
+
+var DefaultConfig = &Config{
+	ServerPath: "D:/XM/bds/bedrock_server.exe",
+}
